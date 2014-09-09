be2be5a02090edaef5de623e7ccd415ba2d122a7
syivanes <sergey.ivanes@gmail.com>
2014-09-08 22:14:24 -0400

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 SampleApp::Application.routes.draw do
   root 'static_pages#home'
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
   match '/contact', to: 'static_pages#contact', via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
